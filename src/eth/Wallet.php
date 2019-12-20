a1329bd28f8ab325b99df828b746aa4276eb6089
leftsky <leftsky@vip.qq.com>
2019-12-21 01:51:44 +0800

    更新erc20交易

--- src/eth/Wallet.php
+++ src/eth/Wallet.php
@@ -15,15 +15,18 @@ use Web3\RequestManagers\HttpRequestManager;
 use Web3\Utils;
 use Web3\Web3;
 use Web3p\EthereumTx\Transaction;
+use xtype\Ethereum\Client as EthereumClient;
 
 class Wallet
 {
-    private $prv_key;
+    public $prv_key;
     private $pub_key;
-    private $address;
+    public $address;
     private $result;
     private $infura_endpoint = 'https://mainnet.infura.io/v3/6a68fac988fb4d899aeeb8d62e037d59';
     private $infura_secret = '84277eb271c348c98a7c88000c0fccbc';
+    private $etherscan_domain = 'http://api-cn.etherscan.com/api?';
+    private $etherscan_key = 'yourapikey';
 
     /**
      * 创建 or 导入一个钱包
@@ -71,14 +74,32 @@ class Wallet
         return $this->result->toString();
     }
 
+    /**
+     * etherscan 查询 ETH 余额
+     * @param string $address 需要查询的地址
+     * @param string $contract 代币合约地址
+     * @return int ETH 余额 （单位wei)
+     */
+    private function balance_erc20_etherscan($address, $contract)
+    {
+        $url = $this->etherscan_domain
+            . "module=account&action=tokenbalance&contractaddress=$contract&address=$address&tag=latest&apikey=$this->etherscan_key";
+        $result = file_get_contents($url);
+        $result = json_decode($result, true);
+        return $result['result'] ?? 0;
+    }
+
     public function balance($contact = null, $platform = 'etherscan')
     {
-        return $this->balance_etherscan($this->address);
+        if ($contact) {
+            return $this->balance_erc20_etherscan($this->address, $contact);
+        } else {
+            return $this->balance_etherscan($this->address);
+        }
     }
 
     /**
      * 发送 ETH 方法
-     * @param string $prv_key 发款钱包私钥
      * @param string $address 收款钱包地址
      * @param double $amount 转账的 ETH 数量。单位 ETH，精度小数点后18位
      * @param string $water_mark 交易备注
@@ -107,9 +128,62 @@ class Wallet
         return $this->result;
     }
 
-    private function send_erc20($address, $amount, $contact = null, $water_mark = 'leftsky')
+    /**
+     * 发送 ERC-20 代币方法
+     * @param string $address 收款地址
+     * @param double $amount 转账的金额
+     * @param string $contact 代币地址
+     * @param int $decimals 小数点位数
+     * @param string $water_mark 备注
+     * @return mixed
+     */
+    private function send_erc20($address, $amount, $contact, $decimals, $water_mark = 'leftsky')
     {
-        return $txid ?? "";
+        // 将汇率转换成字符串
+        $decimals_str = "";
+        while (strlen($decimals_str) < $decimals) {
+            $decimals_str .= "0";
+        }
+
+        // 处理收款地址字符串
+        $to_address = str_replace("0x", "", $address);
+
+        // 链接以太坊节点
+        $client = new EthereumClient([
+            'base_uri' => $this->infura_endpoint,
+            'timeout' => 30,
+        ]);
+
+        // 导入钱包私钥
+        $client->addPrivateKeys([$this->prv_key]);
+
+        // 创建自己的交易
+        $trans = [
+            "from" => $this->address,
+            "to" => $contact,
+            "value" => "0x0",
+        ];
+        // 写入收款地址
+        $trans['data'] = "0x" . "a9059cbb"
+            . "000000000000000000000000$to_address";
+
+        // 写入金额
+        $num = Utils::toHex($amount . $decimals_str);
+        while (strlen($num) != 64) {
+            $num = "0" . $num;
+        }
+        $trans['data'] .= $num;
+
+        // 设定你的手续费
+        $gasPrice = $client->eth_gasPrice();
+        $trans['gasPrice'] = $gasPrice;
+        $gas = 3000000000000000 / hexdec($gasPrice);
+        $trans['gas'] = dechex($gas);
+        $trans['nonce'] = $client->eth_getTransactionCount($this->address, 'pending');
+        $txid = $client->sendTransaction($trans);
+
+        $receipt = $client->eth_getTransactionReceipt($txid);
+        return $txid;
     }
 
     /**
@@ -117,15 +191,16 @@ class Wallet
      * @param string $address 收款地址
      * @param double $amount 金额
      * @param null $contact 合约地址，为 null 或 不填 时为 ETH
+     * @param int $decimals 代币的小数点数
      * @param string $water_mark 交易备注
      * @return string
      */
-    public function send($address, $amount, $contact = null, $water_mark = 'leftsky')
+    public function send($address, $amount, $contact = null, $decimals = 0, $water_mark = 'leftsky')
     {
         if (!$contact) {
             return $this->send_eth($address, $amount, $water_mark);
         } else {
-            return $this->send_erc20($address, $amount, $contact, $water_mark);
+            return $this->send_erc20($address, $amount, $contact, $decimals, $water_mark);
         }
     }
 
